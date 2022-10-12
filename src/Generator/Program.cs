1a5f828d053afb06a036f2835a51657594f441b2
Rafael Jenz <git@rafaeljenz.de>
2022-10-12 13:05:47 +0200

    [ ADD ] Basic setup

--- src/Generator/Program.cs
+++ src/Generator/Program.cs
@@ -1,11 +1,36 @@
 ﻿namespace Generator;
 
-using System;
+using Configurations;
+using Logging;
 
 internal static class Program
 {
-    private static void Main(string[] args)
+    private const string PROJECT_WEBSITE_CONFIG = "website.json";
+    private static readonly Logger LOGGER = Logger.GetInstance();
+
+    private static int Main(string[] args)
     {
-        
+        LOGGER.Configurate(nameof(Generator));
+
+        if (args.Length != 1)
+        {
+            LOGGER.Error("Please provide a project folder when calling Generator. For example: generator.dll \"websites/websiteA\"");
+            return 1;
+        }
+
+        string pathToWebsiteProject = args[0];
+        string pathToWebsiteConfiguration = Path.GetFullPath(Path.Combine(pathToWebsiteProject, PROJECT_WEBSITE_CONFIG));
+
+        WebsiteConfiguration? websiteConfig = WebsiteConfiguration.LoadFromFile(pathToWebsiteConfiguration);
+
+        if (websiteConfig == null)
+        {
+            LOGGER.Error($"Failed to load website configuration \"{pathToWebsiteConfiguration}\"");
+            return 2;
+        }
+
+        //TODO: Run Generation
+
+        return 0;
     }
 }
